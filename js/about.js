4ecb2cc808d34bd59d92f00367d50e94b35a7129
suneelsuthar <suneelsuther123@gmail.com>
2022-12-12 01:57:11 +0500

    completed project

--- js/about.js
+++
@@ -1,3 +0,0 @@
-const submitFrom=()=>{
-    alert("Thank you for your message! It’s always our pleasure to have you here.")
-}
---
+++ js/index.js
@@ -0,0 +1,14 @@
+const submitFrom=()=>{
+    alert("Thank you for your message! It’s always our pleasure to have you here.")
+    document.getElementById("name").value=""
+    document.getElementById("lname").value=""
+    document.getElementById("email").value=""
+    document.getElementById("comment").value=""
+
+}
+
+const subscribe =()=>{
+    alert("Welcome")
+    document.getElementById("subscribe").value=""
+
+}
